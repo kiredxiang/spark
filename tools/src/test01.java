1ee1260c39d77b851b9a172d9b31b27adbae2bc0
xiangquanhong <xiangquanhong@tencent.com>
2024-09-26 23:12:46 +0800

    commit1

---
+++ tools/src/test01.java
@@ -0,0 +1,13 @@
+import scala.math.Ordering;
+
+/**
+ * @author xiangquanhong
+ * @create_date 2024/9/26-23:10
+ * @description
+ */
+public class test01 {
+    public static void main(Ordering.String[] args) {
+
+
+    }
+}
