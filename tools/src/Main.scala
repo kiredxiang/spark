5f8e9046e3d79e68186fa089978577956e14feda
xiangquanhong <xiangquanhong@tencent.com>
2024-09-28 10:46:56 +0800

    update 4444

--- tools/src/Main.scala
+++ tools/src/Main.scala
@@ -4,12 +4,9 @@
    @description 
  */object Main {
   def main(args: Array[String]): Unit = {
-
-    println("Hello world!1111")
-    println("Hello world!33333")
-    println("Hello world!44444")
-    println("Hello world!55555")
-
+    println("Hello world!")
+        println("Hello world!1111")
+    println("33333333")
 
   }
 }
