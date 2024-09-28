861e7699855830582434cc5ac1c621144d96fb4e
xiangquanhong <xiangquanhong@tencent.com>
2024-09-28 11:46:05 +0800

    update 6666

--- tools/src/tt.java
+++ tools/src/tt.java
@@ -1,7 +1,13 @@
+import scala.math.Ordering;
+
 /**
  * @author xiangquanhong
  * @create_date 2024/9/28-11:27
  * @description
  */
 public class tt {
+    public static void main2(Ordering.String[] args) {
+
+
+    }
 }
